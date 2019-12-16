d921a0ab0321bbcc993a520e83757bdf9c9240ab
NguyenHaIT2 <nguyenhath97@gmail.com>
2019-12-16 05:57:46 -0800

    ha

---
+++ WebMVCWatchOnline/Models/Entities/Donhang.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVCWatchOnline.Models.Entities
+{
+    public class Donhang
+    {
+        public List<Bill> DonHangChuaDuyet { get; set; }
+        public List<Bill> DonHangDaDuyet { get; set; }
+    }
+}
